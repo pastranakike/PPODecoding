8f6de23c76c322bc521bb78becfb0dca754e5206
Luis Pastrana <pastrana@gatech.edu>
2021-05-05 17:04:06 -0400

    Update train.py

--- train.py
+++ train.py
@@ -22,6 +22,15 @@ check_env(env)
 checkpoint_callback = CheckpointCallback(save_freq=2500, save_path='/content/drive/MyDrive/Graduate/Spring2021/CS7650/final_project/default_hparams',
                                          name_prefix='decoding_policy_ppo_default_hparam')
 
-model = PPO("MlpPolicy", env, verbose=2, tensorboard_log='drive/MyDrive/policy_log', n_epochs=100, ent_coef=0.1, device="cuda")
+model_ppo = PPO("MlpPolicy", env, verbose=2, tensorboard_log='drive/MyDrive/policy_log', n_epochs=100, ent_coef=0.1, device="cuda")
+
+with torch.no_grad():
+    model_ppo.policy.mlp_extractor.policy_net[0].weight = torch.nn.Parameter(torch.diag(torch.ones(512)).cuda())
+    model_ppo.policy.action_net.weight = torch.nn.Parameter(model.lm_head.weight.clone().cuda())
+    model_ppo.policy.mlp_extractor.policy_net[0].bias = torch.nn.Parameter(torch.zeros(512).cuda())
+
+model_ppo.policy.action_net.bias.requires_grad = False
+model_ppo.policy.mlp_extractor.policy_net[0].weight.requires_grad = False
+model_ppo.policy.mlp_extractor.policy_net[0].bias.requires_grad = False
 
 model.learn(total_timesteps=100000000, callback=checkpoint_callback)
